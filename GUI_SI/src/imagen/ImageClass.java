ece1a88a1530f92b8a62c65b16c271251adafb56
LuisOrta <thelycan1992@gmail.com>
2014-10-09 18:53:41 +0100

    Avanzada la clase ImageClass

--- GUI_SI/src/imagen/ImageClass.java
+++ GUI_SI/src/imagen/ImageClass.java
@@ -5,7 +5,10 @@
  */
 package imagen;
 
-import java.util.ArrayList;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 
 /**
@@ -14,20 +17,37 @@ import javax.swing.ImageIcon;
  */
 public class ImageClass {
     //ATRIBUTOS
-    private int img_size;
-    private int length;
-    private int width;
-    private byte image[];
-    private int colorValues[];
+    private int img_size; //Tamaño de imagen
+    private int length; //Largo de la imagen
+    private int width; //Ancho de la imagen
+    private byte image[]; //Array de bytes de la imagen
+    private int colorValues[];  //Array de colores del histograma
     
     
     
-    public ImageClass(String path){  //Carga la imagen de la ruta seleccionada
-        //CALCULAR LENGTH Y WIDTH//
-        colorValues = new int[255];
+    public ImageClass(String path){  //CONSTRUCTOR:Carga la imagen de la ruta seleccionada
+   
         image = new byte[length*width];
-        //RELLENAR IMAGEN CON EL MAPA DE BITS DE LA RUTA
+        colorValues = new int[255];
+        
+        try{
+            BufferedImage imagen2 = ImageIO.read(new File(path)); 
+            ByteArrayOutputStream baos = new ByteArrayOutputStream(); 
+            ImageIO.write(imagen2, "png", baos); 
+            image=baos.toByteArray();
+            
+
+            //Calcular alto, ancho y tamaño//
+            length = imagen2.getHeight(null);
+            width = imagen2.getWidth(null);
+            img_size = length*width;
+            } 
+        catch(Exception e){ 
+            System.out.println("Some problem has occurred. Please try again"); 
+            }
+
     };
+    
     
     public ImageIcon getImage(){  //Obtiene un ImageIcon del array de bytes.
         ImageIcon image2 = new ImageIcon(image, "TIFF");
@@ -44,9 +64,26 @@ public class ImageClass {
         return colorValues;
     };
     
-    public int pos(int x, int y){
+    
+    
+    public int get_pos(int x, int y){ //Devuelve la posicion de la imagen de coordenadas XY
         return (x-1)*img_size+y-1;
     }
     
+    
+    
+    public int get_size(){  // Devuelve tamaño de imagen
+        return img_size;
+    }
+    
+    
+    public int get_length(){  // Devuelve largo de la imagen
+        return length;
+    }
+    
+    
+    public int get_width(){  // Devuelve ancho de la imagen
+        return width;
+    }
     
 }
