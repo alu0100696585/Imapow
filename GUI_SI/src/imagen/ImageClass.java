a5a65e875f5568f03b73ce13e5067b4d0d71fc54
LuisOrta <thelycan1992@gmail.com>
2014-10-15 20:31:54 +0100

    Añadido binarizacion y negativo
    Binarizacion, negativo, cambio de formato de imagen de bytes a int.

--- GUI_SI/src/imagen/ImageClass.java
+++ GUI_SI/src/imagen/ImageClass.java
@@ -10,6 +10,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
+import java.lang.Byte;
 
 /**
  *
@@ -20,37 +21,75 @@ public class ImageClass {
     private int img_size; //Tamaño de imagen
     private int length; //Largo de la imagen
     private int width; //Ancho de la imagen
-    private byte image[]; //Array de bytes de la imagen
+    private int image[]; //Array de enteros de la imagen
     private int colorValues[];  //Array de colores del histograma
     
     
     
     public ImageClass(String path){  //CONSTRUCTOR:Carga la imagen de la ruta seleccionada
    
-        colorValues = new int[255];
+        colorValues = new int[256];
+        byte[] imagebyte;
         
         try{
             //Convertir imagen a array de bytes
             BufferedImage imagen2 = ImageIO.read(new File(path)); 
             ByteArrayOutputStream baos = new ByteArrayOutputStream(); 
             ImageIO.write(imagen2, "png", baos); 
-            image=baos.toByteArray();
+            imagebyte=baos.toByteArray();
             
-
+            
             //Calcular alto, ancho y tamaño//
             length = imagen2.getHeight(null);
             width = imagen2.getWidth(null);
             img_size = length*width;
+            
+            
+            //Convertir a array de enteros          
+            Byte b;
+            for(int i=0;i<img_size;i++){
+                b = imagebyte[i];
+                image[i] = b.intValue();
+            }
+            
             } 
         catch(Exception e){ 
             System.out.println("Some problem has occurred. Please try again"); 
             }
-
     };
     
     
+    public ImageClass(int[] img, int l, int w){ // CONSTRUCTOR: array de int
+        img_size = l * w; //Tamaño de imagen
+        length = l; //Largo de la imagen
+        width = w; //Ancho de la imagen
+        image = img; //Array de bytes de la imagen
+        colorValues = new int[256];
+    }
+    
+    
+    public ImageClass(ImageClass other){   // CONSTRUCTOR DE COPIA
+        img_size = other.img_size; //Tamaño de imagen
+        length = other.length; //Largo de la imagen
+        width = other.width; //Ancho de la imagen
+        image = other.image; //Array de bytes de la imagen
+        colorValues = other.colorValues;
+    }
+    
+    
+    public byte[] imageToByte(){  // Convierte el array de bytes de la imagen en un array de enteros
+        byte[] b = new byte[img_size];
+        Byte[] img_Byte = new Byte[img_size];
+        for(int i=0;i<img_size;i++){
+            img_Byte[i] = (byte)image[i];
+            b[i] = img_Byte[i].byteValue();
+        }
+        return b;
+    }
+    
+    
     public ImageIcon getImage(){  //Obtiene un ImageIcon del array de bytes.
-        ImageIcon image2 = new ImageIcon(image, "TIFF");
+        ImageIcon image2 = new ImageIcon(this.imageToByte(), "TIFF");
         return image2;
     };
     
@@ -94,8 +133,60 @@ public class ImageClass {
         
         
     public void showHistogram(){ // Muestra el histograma de color de la imagen
-        
+        //SACAR VENTANA???
     }
     
+    public int imgBrightness(){ // Devuelve el brillo (media de color) de la imagen
+        int suma = 0;
+        for(int i=0;i<img_size;i++){
+            suma = suma + image[i];
+        }
+        return (suma/img_size);           
+    }
 
+    
+    
+    public double imgContrast(){  // Devuelve el contraste (Desv. Tipica)
+        int average = this.imgBrightness();
+        double contrast = 0;
+        
+        for(int i=0;i<img_size;i++){
+            contrast = contrast + (image[i] - average) * (image[i] - average);
+        }
+        contrast = contrast / img_size;
+        contrast = Math.sqrt(contrast);
+        return contrast;
+    }
+    
+    
+    
+    public int[] imgNegative(){  // Devuelve el negativo (complemento) de la imagen
+        int[] neg_img = new int[img_size];
+        int L = 255;
+        
+        for (int i=0;i<img_size;i++){
+            neg_img[i] = image[i] - L;
+        }
+        return neg_img;
+    }
+    
+    
+    public int[] imgBinary(int threshold){   // Devuelve la imagen binarizada a partir de un umbral
+        int[] bin_img = new int[img_size];
+        
+        for (int i=0;i<img_size;i++){
+            if (image[i] >= threshold)
+                bin_img[i] = 255;
+            else bin_img[i] = 0;
+        }
+        return bin_img;
+    }
+       
+    
+    public int[] imgSetBrightness(int bright){
+        int[] br_img = new int[img_size];
+        
+    }
 }
+
+
